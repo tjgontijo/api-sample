0be8ab604cc506a90f2d6bbd1d070f7995ecbb81
Thiago Gontijo <tjgontijo@gmail.com>
2023-08-04 21:07:55 -0300

    Update register.ts

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,25 +1,25 @@
-import { registerUseCase } from '@/use-cases/register'
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
 
-export async function registerUser ( request : FastifyRequest, reply: FastifyReply ) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
         name: z.string(),
         email: z.string().email(),
-        password: z.string().min(8)
+        password: z.string().min(6),
     })
 
     const { name, email, password } = registerBodySchema.parse(request.body)
-    
-    try{
+
+    try {
         await registerUseCase({
             name,
             email,
-            password
+            password,
         })
-    }catch(err) {
+    } catch (err) {
         return reply.status(409).send()
     }
-    
+
     return reply.status(201).send()
 }
