1174710341b1ce9b3e5452fb51b878cb2ce64e44
Thiago Gontijo <tjgontijo@gmail.com>
2023-08-05 12:56:09 -0300

    Handler Global Error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -27,8 +27,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         if (err instanceof UserAlreadyExistsError){
             return reply.status(409).send({ message: err.message})
         }
-
-        return reply.status(500).send() // TODO: Fix me
+        
+        throw err
     }
 
     return reply.status(201).send()
