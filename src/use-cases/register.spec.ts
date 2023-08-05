e274894f54eb7d582e6d727569221199887a3f93
Thiago Gontijo <tjgontijo@gmail.com>
2023-08-05 16:11:22 -0300

    Update register.spec.ts

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,59 @@
-import { expect, test } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { compare } from 'bcryptjs'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('Check if it works', () => {
-    expect(2+2).toBe(4)
+describe('Register Use Case', () => {
+    it('should to register', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        const { user } = await registerUseCase.handle({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+    it('should hash user password upon registration', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        const { user } = await registerUseCase.handle({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+
+    it('should not be able to register with same email twice', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        const email = 'johndoe@example.com'
+
+        await registerUseCase.handle({
+            name: 'John Doe',
+            email,
+            password: '123456',
+        })
+
+        expect(() =>
+            registerUseCase.handle({
+                name: 'John Doe',
+                email,
+                password: '123456',
+            })
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+    })
 })
