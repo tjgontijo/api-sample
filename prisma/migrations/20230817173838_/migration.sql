e2f86d28878d7c447a668e5e14c1750b67d23e43
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-18 15:41:03 -0300

    Create migration.sql

---
+++ prisma/migrations/20230817173838_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `percentage` on the `actions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "actions" DROP COLUMN "percentage";
