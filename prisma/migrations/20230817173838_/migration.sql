8672cbe460971abe4d5b1301ddef5a99d90d2967
Thiago Gontijo <tjgontijo@gmail.com>
2023-08-19 22:12:22 -0300

    database modeling

--- prisma/migrations/20230817173838_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `percentage` on the `actions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "actions" DROP COLUMN "percentage";
