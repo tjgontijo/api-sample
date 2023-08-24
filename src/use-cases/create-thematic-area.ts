015e0d1f4acadbb2aff08478bf99a21f7d290497
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-24 14:51:14 -0300

    Update create-thematic-area.ts

--- src/use-cases/create-thematic-area.ts
+++ src/use-cases/create-thematic-area.ts
@@ -1,28 +1,25 @@
-import { CreateThematicAreasRepository } from '@/repositories/thematic-areas-repository'
+import { ThematicAreasRepository } from '@/repositories/thematic-areas-repository'
 import { ThematicArea } from '@prisma/client'
 
 interface CreateThematicAreaUseCaseRequest {
   name: string
-  short_name: string
-  percentage: number
+  short_name: string 
 }
 interface CreateThematicAreaUseCaseResponse {
     thematicArea: ThematicArea 
 }
 
 export class CreateThematicAreaUseCase {
-    constructor(private createThematicAreaRepository: CreateThematicAreasRepository) { }
+    constructor(private createThematicAreaRepository: ThematicAreasRepository) { }
 
     async handle({
         short_name,
         name,
-        percentage,
     }: CreateThematicAreaUseCaseRequest): Promise<CreateThematicAreaUseCaseResponse> {
 
         const thematicArea = await this.createThematicAreaRepository.create({
             short_name,
             name,
-            percentage,
         })
         return {
             thematicArea,
