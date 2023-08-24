e61878a9664d815e054ec9a2cdaad6240413fd03
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-24 14:51:20 -0300

    Update in-memory-create-thematic-area.ts

--- src/repositories/in-memory/in-memory-create-thematic-area.ts
+++ src/repositories/in-memory/in-memory-create-thematic-area.ts
@@ -1,21 +1,19 @@
 import { Prisma, ThematicArea } from '@prisma/client'
-import { CreateThematicAreasRepository } from '../thematic-areas-repository'
+import { ThematicAreasRepository } from '../thematic-areas-repository'
 import { randomUUID } from 'node:crypto'
 
-export class InMemoryCreateThematicAreaRepository implements CreateThematicAreasRepository {
+export class InMemoryThematicAreasRepository implements ThematicAreasRepository {
     public items: ThematicArea[] = []
     
     async create(data: Prisma.ThematicAreaUncheckedCreateInput) {
         const thematicArea = {
             id: randomUUID(),
             short_name: data.short_name,
-            name: data.name,
-            percentage: data.percentage,           
+            name: data.name,                   
             created_at: new Date(),
         }
 
         this.items.push(thematicArea)
-
         return thematicArea
     }
 }
