d5915d2594a10aa341314d1a9e92e561aff89b2e
Thiago Gontijo <tjgontijo@gmail.com>
2023-08-24 21:01:09 -0300

    Update create-thematic-area.spec.ts

--- src/use-cases/create-thematic-area.spec.ts
+++ src/use-cases/create-thematic-area.spec.ts
@@ -1,7 +1,6 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryThematicAreasRepository } from '@/repositories/in-memory/in-memory-create-thematic-area'
 import { CreateThematicAreaUseCase } from './create-thematic-area'
-import { ThematicAreaAlreadyExistsError } from './errors/thematic-area-already-exists'
 
 let ThematicAreasRepository: InMemoryThematicAreasRepository
 let sut: CreateThematicAreaUseCase
@@ -16,24 +15,9 @@ describe('Thematic Area Use Case', () => {
 
         const { thematicArea } = await sut.handle({
             short_name: 'ECV',
-            name: 'Enfrentamento a Criminalidade Violenta',            
+            name: 'Enfrentamento a Criminalidade Violenta',
         })
 
         expect(thematicArea.id).toEqual(expect.any(String))
-    }),
-    it('should not be able to register with same short_name twice', async () => {
-
-        const short_name = 'ECV'
-        await sut.handle({
-            short_name,
-            name: 'Enfrentamento a Criminalidade Violenta',                        
-        })
-
-        await expect(() =>
-            sut.handle({
-                short_name,
-                name: 'Enfrentamento a Criminalidade Violenta',                
-            })
-        ).rejects.toBeInstanceOf(ThematicAreaAlreadyExistsError)
     })
 })
