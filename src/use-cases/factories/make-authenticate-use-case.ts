018b8ee2745b59e67d301f32d53aebd60ac5c5d6
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-29 16:04:10 -0300

    Update make-authenticate-use-case.ts

--- src/use-cases/factories/make-authenticate-use-case.ts
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -1,7 +1,6 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { AuthenticateUseCase } from '../authenticate'
 
-
 export function makeAuthenticateUseCase() {
     const usersRepository = new PrismaUsersRepository()
     const authenticateUseCase = new AuthenticateUseCase(usersRepository)
