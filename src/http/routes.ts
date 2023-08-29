efc4ea231bec385aa7c4ad7272ea5ea8674a9973
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-08-29 16:04:18 -0300

    Update routes.ts

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { createThematicArea } from './controllers/create-thematic-area'
+import { fetchThematicArea } from './controllers/fetch-thematic-area'
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
     app.post('/sessions', authenticate)
     app.post('/thematic_area', createThematicArea)
+    app.get('/thematic_area', fetchThematicArea)
 }
