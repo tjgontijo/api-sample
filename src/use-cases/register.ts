0dc242a8853a775b44595cab699b85e7c09691bd
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-01 16:26:53 -0300

    Update register.ts

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,17 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
+  stateId: string
+}
+
+interface RegisterUseCaseResponse {
+    user: User
 }
 
 export class RegisterUseCase {
@@ -15,8 +21,9 @@ export class RegisterUseCase {
         name,
         email,
         password,
-    }: RegisterUseCaseRequest) {
-
+        stateId,
+    }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+        console.log(stateId)
         const password_hash = await hash(password, 6)
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
@@ -28,8 +35,7 @@ export class RegisterUseCase {
             name,
             email,
             password_hash,
-            roles: {},
-            state: {}
+            stateId,
         })
         return {
             user,
