03c3ea91f99e2ccfd579145a91b9563f0a1add44
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-01 16:27:02 -0300

    Update register.ts

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -6,11 +6,14 @@ import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-cas
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
         name: z.string(),
+        stateId: z.string(),
         email: z.string().email(),
         password: z.string().min(6),
     })
 
-    const { name, email, password } = registerBodySchema.parse(request.body)
+    const { stateId , name, email, password } = registerBodySchema.parse(request.body)
+
+    console.log(request.body)
 
     try {
         
@@ -20,8 +23,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
             name,
             email,
             password,
+            stateId
         })
-    
+            
     } catch (err) {
 
         if (err instanceof UserAlreadyExistsError){
