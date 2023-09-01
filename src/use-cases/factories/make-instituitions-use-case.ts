49f920bae947f59a91ca0ff080e50271720e46f3
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-09-01 18:18:18 -0300

    Delete make-instituitions-use-case.ts

--- src/use-cases/factories/make-instituitions-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaInstituitionsRepository } from '@/repositories/prisma/prisma-intituitions-repository copy'
-import { FetchiInstituitionUseCase } from '../fetch-instituitions'
-
-export function makeFetchInstituitionsUseCase() {
-    const instituitionsRepository = new PrismaInstituitionsRepository()
-    const fetchInstituitions = new FetchiInstituitionUseCase(instituitionsRepository)
-
-    return fetchInstituitions
-}
