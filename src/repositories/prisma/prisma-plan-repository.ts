76dc4015c4cc7e0057234a35dc061fa8fba9a09b
Thiago Gontijo <tjgontijo@gmail.com>
2023-09-25 20:53:29 -0300

    Create prisma-plan-repository.ts

---
+++ src/repositories/prisma/prisma-plan-repository.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { PlanRepository } from '../plan-repository'
+
+export class PrismaPlanRepository implements PlanRepository {
+
+    async fetchAll() {
+        const plans = await prisma.plan.findMany({
+            include: { thematicArea: true }
+        })
+        return plans
+    }
+
+    async create(data: Prisma.PlanUncheckedCreateInput) {
+        const response = await prisma.plan.create({
+            data,
+        })
+        return response
+    }
+}
