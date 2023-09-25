05d9d82058b108830328a72dcb24d0822f37a762
Thiago Gontijo <tjgontijo@gmail.com>
2023-09-25 20:53:31 -0300

    Update make-create-action-use-case.ts

--- src/use-cases/factories/make-create-action-use-case.ts
+++ src/use-cases/factories/make-create-action-use-case.ts
@@ -1,9 +1,9 @@
-import { PrismaActionsRepository } from '@/repositories/prisma/prisma-actions-repository'
-import { CreateActionUseCase } from '../create-action'
+import { PrismaPlanRepository } from '@/repositories/prisma/prisma-plan-repository'
+import { CreatePlanUseCase } from '../create-action-plan'
 
-export function makeCreateActionUseCase() {
-    const actionRepository = new PrismaActionsRepository()
-    const createAction = new CreateActionUseCase(actionRepository)
+export function makeCreatePlanUseCase() {
+    const planRepository = new PrismaPlanRepository()
+    const createPlan = new CreatePlanUseCase(planRepository)
 
-    return createAction
+    return createPlan
 }
